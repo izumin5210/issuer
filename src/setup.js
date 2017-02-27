09fed110db20e9b15c722efa801774493a3f137b
izumin5210 <masayuki@izumin.info>
2017-02-27 23:41:48 +0900

    Introduce flow-runtime for development

--- src/setup.js
+++ src/setup.js
@@ -4,6 +4,7 @@ import React from 'react'
 global.React = React
 
 if (process.env.NODE_ENV !== 'production') {
-  global.assert = require('power-assert'); // eslint-disable-line
-  global.Perf = require('react-addons-perf'); // eslint-disable-line
+  require('regenerator-runtime/runtime')
+  global.assert = require('power-assert') // eslint-disable-line
+  global.Perf = require('react-addons-perf') // eslint-disable-line
 }
